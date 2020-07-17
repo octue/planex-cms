0fdcd1dc14267c9a195908a583ef64ad5ed33eed
Tom Clark <tom@octue.com>
2020-07-17 17:16:44 +0100

    FIX Correct decoding of the kombu fernet key

--- scripts/generate_kombu_fernet_key.py
+++ scripts/generate_kombu_fernet_key.py
@@ -2,4 +2,5 @@ import os
 from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
-os.environ["KOMBU_FERNET_KEY"] = str(key)
+print("Generated key: ", key.decode("utf-8"))
+os.environ["KOMBU_FERNET_KEY"] = key.decode("utf-8")
