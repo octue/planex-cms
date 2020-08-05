c373b5fa2b24aafb4b43cfb06caa38bcefb3592e
Tom Clark <tom@octue.com>
2020-08-05 16:51:15 +0100

    FIX Travis now working and correct key generation procedure stored in script

--- scripts/generate_kombu_fernet_key.py
+++ scripts/generate_kombu_fernet_key.py
@@ -2,5 +2,9 @@ import os
 from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
-print("Generated key: ", key.decode("utf-8"))
+print("-----------\nGenerated key:")
+print(key.decode("utf-8"))
 os.environ["KOMBU_FERNET_KEY"] = key.decode("utf-8")
+print("-----------\nAdd this as a secret to the travis file by using the travis CLI with the following command:")
+print(f'travis encrypt --add --pro KOMBU_FERNET_KEY={key.decode("utf-8")}')
+print("-----------")
